87437811dc98c80d4e5d3f5d1af153339eabc61e
02madmax <maxlamthach10@gmail.com>
2023-09-21 15:43:54 -0600

    finished javascript code and added more html

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-const dayjs = require("dayjs");
+const saveButton = document.getElementById("save-btn");
+const past = document.getElementById("past");
+const present = document.getElementById("present");
+const future = document.getElementById("future");
+const rows = document.getElementsByClassName("row");
+
 $(function () {
   $(".saveBtn").on("click", function () {
     const timeBlock = $(this).closest(".time-block").attr("id");
@@ -10,9 +15,52 @@ $(function () {
     localStorage.setItem(timeBlock, userInput);
   });
 
+  // Get the current hour using Day.js
   const currentHour = dayjs().hour();
 
+  $(".time-block").each(function () {
+    const timeBlock = parseInt($(this).attr("id").split("-")[1]);
+    if (timeBlock < currentHour) {
+      $(this).addClass("past");
+    } else if (timeBlock === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
 
+  $(".time-block").each(function () {
+    const timeBlock = $(this).attr("id");
+    const userInput = localStorage.getItem(timeBlock);
+    if (userInput) {
+      $(this).find("textarea").val(userInput);
+    }
+  });
+
+  // Get the current date using Day.js and format it
+  const currentDay = dayjs().format("dddd, MMMM D");
+  $("#currentDay").text(currentDay);
+});
+
+  $(document).ready(function () {
+    const TimeBlocks = $(".time-block");
+    const savedEvents = JSON.parse(localStorage.getItem("savedEvents")) || [];
+    timeBlocks.each(function () {
+      const timeBlock = $(this);
+      const hour = parseInt(timeBlock.attr("id").split("-")[1]);
+      const eventInput = timeBlock.find("textarea");
+
+      eventInput.val(savedEvents[hour]);
+
+      timeBlock.find(".saveBtn").on("click", function (event) {
+        event.preventDefault();
+        const eventText = eventInput.val();
+        savedEvents[hour] = eventText;
+        localStorage.setItem("savedEvents", JSON.stringify(savedEvents));
+      });
+    });
+  });
+  
 
 
 
@@ -34,4 +82,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
